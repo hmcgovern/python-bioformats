e37157f1de1ce8893f2387235dc4cca1b44ef8ab
hmcgovern <56073830+hmcgovern@users.noreply.github.com>
2019-11-22 15:14:27 +0100

    Update setup.py
    Removes `"javabridge">=1` from `install_requires`, presupposing an existing, functional install. Current pip release of javabridge does not install correctly. Had to build from git repo.

--- setup.py
+++ setup.py
@@ -20,8 +20,7 @@ setuptools.setup(
     },
     install_requires=[
         "boto3",
-        "future",
-        "javabridge>=1.0"
+        "future"
     ],
     license="GPL License",
     long_description="""Python-bioformats is a Python wrapper for Bio-Formats, a standalone Java library for reading
